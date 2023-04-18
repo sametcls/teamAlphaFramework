d0e44eea7329d1e961598c821fad65283e890b70
sametcls <117773059+sametcls@users.noreply.github.com>
2023-04-17 18:18:58 -0700

    firs commit

---
+++ src/test/java/MyClass.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.awt.*;
+import java.time.Duration;
+
+public class MyClass {
+
+    @Test
+    public void ActionClassTest () throws AWTException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get("https://google.com");
+
+        //Actions actions = new Actions(driver);
+    }
+}
